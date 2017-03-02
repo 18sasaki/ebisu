72a743540c2ca770dbdf649850292216d421bfee
18sasaki <18sasaki@gmail.com>
2017-03-03 00:47:08 +0900

    エラーページ追加

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,5 +1,6 @@
 class InvitationsController < ApplicationController
   def show
     @guest = Guest.find_by(id_hash: params[:id_hash])
+    render file: "/public/unknown.html" unless @guest
   end
 end
