5aff7037461b26a83eda6f383cbb195275c6c1e5
18sasaki <18sasaki@gmail.com>
2017-07-17 01:38:06 +0900

    返信フォームをざっくり作った

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -3,4 +3,30 @@ class InvitationsController < ApplicationController
     @guest = Guest.find_by(id_hash: params[:id_hash])
     render file: "/public/unknown.html" unless @guest
   end
+
+  def reply
+    @guest = Guest.find_by(id_hash: params[:id_hash])
+p ">>>>>>>>>>>>>>>"
+p @guest
+p ">>>>>>>>>>>>>>>"
+    @guest.attend_bit = params[:attend_bit]
+p @guest.attend_bit
+p ">>>>>>>>>>>>>>>"
+    @guest.guest_message = params[:guest_message]
+
+    respond_to do |format|
+      if @guest.save
+        format.html { redirect_to 'show', notice: "返信ありがとう！！" }
+        format.json { render :show, status: :ok }
+      else
+        format.html { render :show }
+        format.json { render json: @guest.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    def set_guest
+      @guest = Guest.find_by(id_hash: params[:id_hash])
+    end
 end
