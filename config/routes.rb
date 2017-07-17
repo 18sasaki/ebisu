6c43bf5b8b4eb0702265b7c847eb7bc5c80d16f7
18sasaki <18sasaki@gmail.com>
2017-07-18 00:19:44 +0900

    validateかかった後に再読み込みされた場合対策

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'invitations/:id_hash',          to: 'invitations#show'
   post 'invitations/:id_hash/reply',   to: 'invitations#reply'
+  get 'invitations/:id_hash/reply' => redirect("/invitations/%{id_hash}") #validateかかった後に再読み込みされた場合対策
 
   resources :guests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
