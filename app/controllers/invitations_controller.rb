34e30982f14a8f65badf2133a11acec93ebefe8f
18sasaki <18sasaki@gmail.com>
2017-08-04 23:00:19 +0900

    invitations_controllerを整理

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,29 +1,20 @@
 class InvitationsController < ApplicationController
+  before_action :set_guest
+
   def show
-    @guest = Guest.find_by(id_hash: params[:id_hash])
-    @attend_type = case @guest.attend_bit
-                   when nil then "none"
-                   when true then "attend"
-                   when false then "absent"
-                   end
-    render file: "/public/unknown.html" unless @guest
-    render 'show_2'
   end
 
   def reply
-    @guest = Guest.find_by(id_hash: params[:id_hash])
     @guest.attend_bit = params[:attend_bit]
     @guest.guest_message = params[:guest_message]
     @guest.allergy_comment = params[:allergy_comment]
 
     respond_to do |format|
       if @guest.save(context: :by_guest)
-        p ">>>>>>>>>>>>>"
-        p @guest.attributes
         format.html { render :reply, notice: "返信ありがとう！！" }
         format.json { render :reply, status: :ok }
       else
-        format.html { render :show_2 }
+        format.html { render :show }
         format.json { render json: @guest.errors, status: :unprocessable_entity }
       end
     end
@@ -32,5 +23,6 @@ class InvitationsController < ApplicationController
   private
     def set_guest
       @guest = Guest.find_by(id_hash: params[:id_hash])
+      render file: "/public/unknown.html" unless @guest
     end
 end
