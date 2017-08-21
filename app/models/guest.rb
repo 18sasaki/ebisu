e9a7d1e282a79b0b1108fa48a566145d369d65ad
18sasaki <18sasaki@gmail.com>
2017-08-21 17:32:21 +0900

    csvインポートを一時的に上書き

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -6,17 +6,22 @@ class Guest < ApplicationRecord
     CSV.foreach(file.path, headers: true) do |row|
     	begin
 	      # TODO: csvファイルのheaderの最初の列の先頭に「・」が入ってエラーになる件が不明のまま
-	      # TODO: 名寄せか上書きかはじくか
-	      # TODO: 上書きフラグ？
-	      if Guest.find_by(name: row['name'])
-		    	result_hash[:duplication] += 1
-		    else
-		      guest = self.new
-		      guest.attributes = row.to_hash.slice(*updatable_attributes)
-		      guest.id_hash = Digest::MD5.hexdigest('tomo' + guest.name + 'eri')
-		      guest.save!
-		    	result_hash[:success] += 1
-		    end
+	      # 緊急対応で同名があったら上書きさせる
+	     #  if Guest.find_by(name: row['name'])
+		    # 	result_hash[:duplication] += 1
+		    # else
+		    #   guest = self.new
+		    #   guest.attributes = row.to_hash.slice(*updatable_attributes)
+		    #   guest.id_hash = Digest::MD5.hexdigest('tomo' + guest.name + 'eri')
+		    #   guest.save!
+		    # 	result_hash[:success] += 1
+		    # end
+	      guest = Guest.find_by(name: row['name']) || self.new
+p guest
+	      guest.attributes = row.to_hash.slice(*updatable_attributes)
+	      guest.id_hash = Digest::MD5.hexdigest('tomo' + guest.name + 'eri')
+	      guest.save!
+	    	result_hash[:success] += 1
 	    rescue => e
 	    	result_hash[:error_message] << e
 	    	result_hash[:error] += 1
